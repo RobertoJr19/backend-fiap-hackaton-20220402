72f57c2dd6471c68de64b42c5ff36c3b1da1e707
Marcelo A. de Góes <marcelogoes@gmail.com>
2022-04-02 12:20:23 -0300

    alteração para conectar no SQL Cloud

--- db.py
+++ db.py
@@ -2,30 +2,37 @@ import os
 import pymysql
 from flask import jsonify
 
-db_user = os.environ.get('CLOUD_SQL_USERNAME')
-db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_local_host = os.environ.get('DB_LOCAL_HOST')
-db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+# db_user = os.environ.get('CLOUD_SQL_USERNAME')
+# db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+# db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+# db_local_host = os.environ.get('DB_LOCAL_HOST')
+# db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+db_user = CLOUD_SQL_USERNAME
+db_password = CLOUD_SQL_PASSWORD
+db_name = CLOUD_SQL_DATABASE_NAME
+db_connection_name = CLOUD_SQL_CONNECTION_NAME
+
 
 def open_connection():
     try:
         if db_connection_name:
-            unix_socket = '/cloudsql/{}'.format(db_connection_name)
+            #unix_socket = '/cloudsql/{}'.format(db_connection_name)
             conn = pymysql.connect(user=db_user, password=db_password,
-                                unix_socket=unix_socket, db=db_name,
-                                cursorclass=pymysql.cursors.DictCursor
-                                )
-            print ('tchau')
+                                   host=CLOUD_SQL_CONNECTION_NAME, db=db_name,
+                                   cursorclass=pymysql.cursors.DictCursor
+                                   )
+            print('Cloud SQL')
         else:
-            print ('oi')
+            print('On Prem SQL')
             conn = pymysql.connect(user=db_user, password=db_password,
-                                host=db_local_host, db=db_name,cursorclass=pymysql.cursors.DictCursor)
+                                   host=db_local_host, db=db_name, cursorclass=pymysql.cursors.DictCursor)
 
     except pymysql.MySQLError as e:
         print(e)
 
     return conn
+
 
 def get_songs():
     conn = open_connection()
@@ -33,7 +40,7 @@ def get_songs():
         result = cursor.execute('SELECT * FROM songs;')
         songs = cursor.fetchall()
         if result > 0:
-           got_songs = jsonify(songs)
+            got_songs = jsonify(songs)
 
         else:
             got_songs = 'Nenhuma Musica Cadastrada na Playlist'
